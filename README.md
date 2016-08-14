5e1c10acc0b36b6aa3151c28b033f168484fce5e
asrocha92 <asrocha92@gmail.com>
2016-08-14 12:44:57 -0300

    #4 Primeira fase, Criados os EJBs ProcessEntrega e ProcessVenda.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #1 lab141107
 #2 Servlet e entrega e venda.
 #3 Classes venda e entrega;
+#4 Criados os EJBs ProcessEntrega e ProcessVenda
---
+++ lab141104/src/main/java/br/univel/EJB/ProcessEntrega.java
@@ -0,0 +1,19 @@
+package br.univel.EJB;
+
+import javax.ejb.Stateless;
+
+import br.univel.classe.Entrega;
+
+/**
+ * 
+ * @author Alex Santos
+ *
+ */
+
+@Stateless
+public class ProcessEntrega {
+
+	public void processarEntrega(Entrega e) {
+
+	}
+}
